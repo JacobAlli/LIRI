ce3b22efe446571fdefb4e00bdad6e4876018362
JacobAlli <jallison42@gmail.com>
2017-11-24 17:13:55 -0600

    add movie and spotify functionality

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//set up node package requirements
 
 var keys = require('./keys.js');
 
 var request = require('request');
 
-
+var omdb = require('omdb');
 
 var twitter = require('twitter');
 
@@ -14,8 +15,6 @@ var client = new twitter({
   	access_token_secret: keys.twitter_access_token_secret
 });
 
-
-
 var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify({
@@ -23,24 +22,29 @@ var spotify = new Spotify({
   	secret: keys.spotify_client_secret
 });
 
-
-
 var fs = require('fs');
 
+// set up command variables
 
-
+//liri commands such as: my-tweets, spotify-this-song, movie-this
 var liriCmd = process.argv[2];
 
-var songName = process.argv.splice(3).join('+');
+//name of the spotify song or movie title depending on liriCmd...
+var name = process.argv.splice(3).join('+');
 
 
+//display tweets function
 function myTweets(){
-	var params = {screen_name: 'ZeeRealJacob'};
+	//parameter for twitter handle
+    var params = {screen_name: 'ZeeRealJacob'};
 
+    //grab the statuses from the users timeline
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
-	  	if (!error) {
+	  	//error handler
+        if (!error) {
 	  		console.log('Successfully loaded Tweets');
 
+            //go through the most recent 20 tweets and log to the console the text and the date created
 	    	for(i=0; i<20; i++){
 	    		console.log('#'+ (i+1) +':  '+ tweets[i].text +'  ||  '+ tweets[i].created_at);
 	    	};
@@ -51,12 +55,15 @@ function myTweets(){
 	});
 };
 
+//spotify song search function passing in the name of the song as an argument
 function spotifyThisSong(songName){
 
+    //request the song information from the spotify api
 	spotify
   	.request('https://api.spotify.com/v1/search?q=*'+songName+'*&type=track')
   	.then(function(data) {
-    	
+
+        //if the search yields no results run the noTrackResults function...
     	if(data.tracks.items.length === 0){
     		noTrackResults();
     	}
@@ -69,19 +76,57 @@ function spotifyThisSong(songName){
     		console.log('-');
 	    	for(i=1; i<data.tracks.items.length -1;i++){
 	    		if(data.tracks.items[i].name.toLowerCase().includes(songName.toLowerCase().replace('+',' '))){
-	    			console.log(data.tracks.items[i].name +'  ||  '+ data.tracks.items[i].artists[0].name+ '  ||  ' + data.tracks.items[i].preview_url + '  ||  Album: ' + data.tracks.items[i].album.name); 
+	    			console.log(data.tracks.items[i].name +'  ||  '+ data.tracks.items[i].artists[0].name+ '  ||  ' + data.tracks.items[i].preview_url + '  ||  Album: ' + data.tracks.items[i].album.name);
     			}
     		};
     	}
   	})
   	.catch(function(err) {
-	    console.error('Error occurred: ' + err); 
+	    console.error('Error occurred: ' + err);
   	});
+};
+
+function movieResults(movieName){
+    request('http://www.omdbapi.com/?apikey=trilogy&t='+movieName+'&plot=full', function(err, data, body){
+        if(err){
+            console.log(err);
+        }
+        var movieData = JSON.parse(data.body);
+        console.log(' ');
+        console.log('Here\'s some info on your movie:');
+        console.log(' ');
+        console.log('Movie Title: '+movieData.Title);
+        console.log('Released: '+movieData.Released);
+
+        checkForRatings('Internet Movie Database', movieData);
+        checkForRatings('Rotten Tomatoes', movieData);
+
+        console.log('Country: '+movieData.Country);
+        console.log('Language: '+movieData.Language);
+        console.log('Plot: '+movieData.Plot);
+        console.log('Cast: '+movieData.Actors);
+        console.log(' ');
+    })
+};
+
+function checkForRatings(vendor, movieData){
+    do{
+        var found = false;
+        for(i=0; i<movieData.Ratings.length; i++){
+            if(movieData.Ratings[i].Source === vendor){
+                console.log(vendor+ ': '+movieData.Ratings[i].Value);
+                return found = true;
+            }
+        }
+        break;
+    }while(!found);
+
+    console.log(vendor+ ': no ratings found for '+vendor);
 };
 
 function noTrackResults(){
 	console.log('No tracks match your search criteria, try the following track instead:');
-	
+
 	spotify
 	.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE')
 	.then(function(data){
@@ -92,8 +137,19 @@ function noTrackResults(){
 	});
 };
 
-// myTweets();
+switch(liriCmd){
+    case 'my-tweets':
+        myTweets();
+        break;
+    case 'spotify-this-song':
+        spotifyThisSong(name);
+        break;
+    case 'movie-this':
+        movieResults(name);
+        break;
+    default:
+        console.log("command is not valid, try again!");
+};
 
 
-spotifyThisSong(songName);
 
